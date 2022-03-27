e128a7314f6111f2ece114ebc3248433b61d07eb
NTT123 <9960521+NTT123@users.noreply.github.com>
2022-03-27 07:12:15 +0000

    new model

--- extract_model.py
+++ extract_model.py
@@ -1,5 +1,5 @@
 import pickle
 
-dic = pickle.load(open("./wavegru_vocoder_1024_v3_1310000.ckpt", "rb"))
+dic = pickle.load(open("./wavegru_vocoder_1024_v3_1330000.ckpt", "rb"))
 del dic["optim_state_dict"]
-pickle.dump(dic, open("./wavegru_vocoder_1024_v3_1310000.ckpt", "wb"))
+pickle.dump(dic, open("./wavegru_vocoder_1024_v3_1330000.ckpt", "wb"))
